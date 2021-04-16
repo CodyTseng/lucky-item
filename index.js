06922be41df903d9639bbe686787cb8824677df8
codytseng <zenan@quqqi.com>
2021-04-16 12:19:21 +0800

    Write class by prototype

--- index.js
+++ index.js
@@ -1,147 +1,144 @@
 
 'use strict';
-module.exports = void 0;
 
-class LuckyItem {
-    constructor(options = {}) {
-        this.random = options.random || _random;
-        this.unique = options.unique === undefined ? true : options.unique;
-    }
+module.exports = new LuckyItem();
+module.exports.LuckyItem = LuckyItem;
 
-    /**
-     * Randomly select some indexs from the array.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {number} count The number of indexs you want to select.
-     * @param {object} options 
-     * @returns Returns the array of lucky indexs.
-     */
-    indexs = function (arr, count, options = {}) {
-        const unique = options.unique === undefined ? this.unique : options.unique;
-        const random = options.random || this.random;
-        count = unique && arr.length < count ? arr.length : count;
+function LuckyItem(options = {}) {
+    this.random = options.random || _random;
+    this.unique = options.unique === undefined ? true : options.unique;
+}
 
-        const indexs = [...new Array(arr.length).keys()];
-        return [...new Array(count)].map(() => unique
-            ? indexs.splice(random(0, indexs.length - 1), 1)[0]
-            : indexs[random(0, indexs.length - 1)]
-        );
-    }
+/**
+ * Randomly select some indexs from the array.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {number} count The number of indexs you want to select.
+ * @param {object} options 
+ * @returns Returns the array of lucky indexs.
+ */
+LuckyItem.prototype.indexs = function (arr, count, options = {}) {
+    const unique = options.unique === undefined ? this.unique : options.unique;
+    const random = options.random || this.random;
+    count = unique && arr.length < count ? arr.length : count;
 
-    /**
-     * Randomly select an index from the array.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {object} options 
-     * @returns Returns the lucky index.
-     */
-    index = function (arr, options = {}) {
-        return this.indexs(arr, 1, options)[0];
-    }
+    const indexs = [...new Array(arr.length).keys()];
+    return [...new Array(count)].map(() => unique
+        ? indexs.splice(random(0, indexs.length - 1), 1)[0]
+        : indexs[random(0, indexs.length - 1)]
+    );
+}
 
-    /**
-     * Randomly select some items from the array.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {number} count The number of items you want to select.
-     * @param {object} options 
-     * @returns Returns the array of lucky items
-     */
-    items = function (arr, count, options = {}) {
-        return this.indexs(arr, count, options).map(index => arr[index]);
-    }
+/**
+ * Randomly select an index from the array.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {object} options 
+ * @returns Returns the lucky index.
+ */
+LuckyItem.prototype.index = function (arr, options = {}) {
+    return this.indexs(arr, 1, options)[0];
+}
 
-    /**
-     * Randomly select an item from the array.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {object} options 
-     * @returns Returns the lucky item.
-     */
-    item = function (arr, options = {}) {
-        return arr[this.index(arr, options)];
-    }
+/**
+ * Randomly select some items from the array.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {number} count The number of items you want to select.
+ * @param {object} options 
+ * @returns Returns the array of lucky items
+ */
+LuckyItem.prototype.items = function (arr, count, options = {}) {
+    return this.indexs(arr, count, options).map(index => arr[index]);
+}
 
-    /**
-     * Randomly select some indexs from the array based on weights.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {string | Function} weights The field name of weights or a function that returns weights.
-     * @param {number} count The number of indexs you want to select.
-     * @param {object} options 
-     * @returns Returns the array of lucky indexs.
-     */
-    indexsBy = function (arr, weights, count, options = {}) {
-        const unique = options.unique === undefined ? this.unique : options.unique;
-        const random = options.random || this.random;
-        count = unique && arr.length < count ? arr.length : count;
+/**
+ * Randomly select an item from the array.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {object} options 
+ * @returns Returns the lucky item.
+ */
+LuckyItem.prototype.item = function (arr, options = {}) {
+    return arr[this.index(arr, options)];
+}
 
-        const weightsFunc = typeof weights === 'string'
-            ? o => o[weights]
-            : weights;
-        const weightsArr = arr.map(a => weightsFunc(a));
+/**
+ * Randomly select some indexs from the array based on weights.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {string | Function} weights The field name of weights or a function that returns weights.
+ * @param {number} count The number of indexs you want to select.
+ * @param {object} options 
+ * @returns Returns the array of lucky indexs.
+ */
+LuckyItem.prototype.indexsBy = function (arr, weights, count, options = {}) {
+    const unique = options.unique === undefined ? this.unique : options.unique;
+    const random = options.random || this.random;
+    count = unique && arr.length < count ? arr.length : count;
 
-        let totalWeights = weightsArr.reduce((acc, cur) => acc + cur, 0);
-        const arrWithIndex = weightsArr.map((value, index) => [index, value]);
+    const weightsFunc = typeof weights === 'string'
+        ? o => o[weights]
+        : weights;
+    const weightsArr = arr.map(a => weightsFunc(a));
 
-        return [...new Array(count)].map(() => {
-            const randomInt = random(1, totalWeights);
-            let startWeight = 0;
-            const find = arrWithIndex.find(a => {
-                startWeight += a[1];
-                return startWeight >= randomInt;
-            });
+    let totalWeights = weightsArr.reduce((acc, cur) => acc + cur, 0);
+    const arrWithIndex = weightsArr.map((value, index) => [index, value]);
 
-            if (unique) {
-                totalWeights -= find[1];
-                arrWithIndex[find[0]][1] = 0;
-            }
-
-            return find[0];
+    return [...new Array(count)].map(() => {
+        const randomInt = random(1, totalWeights);
+        let startWeight = 0;
+        const find = arrWithIndex.find(a => {
+            startWeight += a[1];
+            return startWeight >= randomInt;
         });
-    }
 
-    /**
-     * Randomly select an index from the array based on weights.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {string | Function} weights The field name of weights or a function that returns weights.
-     * @param {object} options 
-     * @returns Returns the lucky index.
-     */
-    indexBy = function (arr, weights, options = {}) {
-        return this.indexsBy(arr, weights, 1, options)[0];
-    }
+        if (unique) {
+            totalWeights -= find[1];
+            arrWithIndex[find[0]][1] = 0;
+        }
 
-    /**
-     * Randomly select some items from the array based on weights.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {string | Function} weights The field name of weights or a function that returns weights.
-     * @param {number} count The number of items you want to select.
-     * @param {object} options 
-     * @returns Returns the array of lucky items.
-     */
-    itemsBy = function (arr, weights, count, options = {}) {
-        return this.indexsBy(arr, weights, count, options).map(index => arr[index]);
-    }
+        return find[0];
+    });
+}
 
-    /**
-     * Randomly select an item from the array based on weights.
-     * 
-     * @param {object[]} arr The array for selection.
-     * @param {string | Function} weights The field name of weights or a function that returns weights.
-     * @param {object} options 
-     * @returns Returns the lucky item.
-     */
-    itemBy = function (arr, weights, options = {}) {
-        return arr[this.indexBy(arr, weights, options)];
-    }
+/**
+ * Randomly select an index from the array based on weights.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {string | Function} weights The field name of weights or a function that returns weights.
+ * @param {object} options 
+ * @returns Returns the lucky index.
+ */
+LuckyItem.prototype.indexBy = function (arr, weights, options = {}) {
+    return this.indexsBy(arr, weights, 1, options)[0];
+}
+
+/**
+ * Randomly select some items from the array based on weights.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {string | Function} weights The field name of weights or a function that returns weights.
+ * @param {number} count The number of items you want to select.
+ * @param {object} options 
+ * @returns Returns the array of lucky items.
+ */
+LuckyItem.prototype.itemsBy = function (arr, weights, count, options = {}) {
+    return this.indexsBy(arr, weights, count, options).map(index => arr[index]);
+}
+
+/**
+ * Randomly select an item from the array based on weights.
+ * 
+ * @param {object[]} arr The array for selection.
+ * @param {string | Function} weights The field name of weights or a function that returns weights.
+ * @param {object} options 
+ * @returns Returns the lucky item.
+ */
+LuckyItem.prototype.itemBy = function (arr, weights, options = {}) {
+    return arr[this.indexBy(arr, weights, options)];
 }
 
 function _random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-module.exports = new LuckyItem();
-module.exports.LuckyItem = LuckyItem;
